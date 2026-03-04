15a55a0dfcbf767393e42b4341876940839b3070
Matej Knopp <matej.knopp@gmail.com>
2026-03-04 21:33:48 +0100

    Update example.

--- example/package/hook/build.dart
+++ example/package/hook/build.dart
@@ -5,16 +5,17 @@ import 'package:hooks/hooks.dart';
 void main(List<String> args) async {
   await build(args, (input, output) async {
     final packageName = input.packageName;
-    final cbuilder = CBuilder.library(
+    final ninjaBuilder = NinjaBuilder.library(
       name: packageName,
       assetName: '${packageName}_bindings_generated.dart',
       sources: ['src/$packageName.c'],
     );
-    await cbuilder.run(
+    await ninjaBuilder.run(
       input: input,
       output: output,
       logger: Logger('')
         ..level = .ALL
+        // ignore: avoid_print
         ..onRecord.listen((record) => print(record.message)),
     );
   });
